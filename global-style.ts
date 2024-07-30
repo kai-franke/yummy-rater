3028a64f2bdf4ea994a70c5eac878db1ad9754f5
Kai Franke <cosinushh@gmail.com>
2024-07-30 18:27:46 +0200

    add Roboto font

--- global-style.ts
+++ global-style.ts
@@ -1,7 +1,14 @@
 import { css } from "@emotion/react";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500"],
+});
 
 export const global = css`
- body {
+  body {
     background-color: blueviolet;
- }
+    font-family: ${roboto.style.fontFamily};
+  }
 `;
