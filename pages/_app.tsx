79eccbd364437793fdf932cb78c6f4416e0ef7cb
Kai Franke <cosinushh@gmail.com>
2024-07-30 18:13:11 +0200

    add global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,13 @@
 import type { AppProps } from "next/app";
+import { Global } from "@emotion/react";
+
+import { global } from "@/global-style";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return(
+  <>
+    <Global styles={global} />
+    <Component {...pageProps} />
+  </>
+  )
 }
