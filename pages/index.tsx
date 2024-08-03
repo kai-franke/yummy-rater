86c0a2153ead668453a0aa93b26ec0a90c3278b0
Roland-Hufnagel <roland.hufnagel@gmx.de>
2024-08-03 20:35:05 +0200

    add some mui examples

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,41 @@
 import Head from "next/head";
 import styled from "@emotion/styled";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { Box, Button, IconButton, Stack, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  IconButton,
+  Stack,
+  Typography,
+  ToggleButtonGroup,
+  ToggleButton,
+} from "@mui/material";
 import Link from "next/link";
 import SendIcon from "@mui/icons-material/Send";
+import FormatBoldIcon from "@mui/icons-material/FormatBold";
+import FormatItalicIcon from "@mui/icons-material/FormatItalic";
+import FormatUnderlinedIcon from "@mui/icons-material/FormatUnderlined";
+import { useState } from "react";
 
 export default function Home() {
+  const [formats, setFormats] = useState<string[]>([]);
+  const [exclusiveFormats, setExclusiveFormats] = useState<string | null>(null);
+
+  console.log("formats: ", formats);
+  console.log('exclusiveFormats: ', exclusiveFormats);
+  function handleFormatChange(
+    _event: React.MouseEvent<HTMLElement>,
+    updatedFormats: string[]
+  ) {
+    setFormats(updatedFormats);
+  }
+  function handleExclusiveFormatChange(
+    _event: React.MouseEvent<HTMLElement>,
+    updatedFormats: string | null
+  ) {
+    setExclusiveFormats(updatedFormats);
+  }
   return (
     <>
       <Head>
@@ -43,9 +73,7 @@ export default function Home() {
           </GreenButton>
         </Box>
         <Typography variant="h1">Headline 1</Typography>
-
         <Typography variant="h2">Headline 2</Typography>
-
         <Typography variant="h3">Headline 3</Typography>
         <Typography variant="h4" gutterBottom>
           Headline 4
@@ -117,6 +145,50 @@ export default function Home() {
           Ah! in einer flexbox mit direction row werden sie in die höge
           gestreched. Bei display block verhalten sie sich anders
         </Typography>
+        <ButtonGroup variant="text" orientation="vertical">
+          <Button>left</Button>
+          <Button>center</Button>
+          <Button>right</Button>
+        </ButtonGroup>
+        <Stack direction="row">
+          <ButtonGroup variant="contained" aria-label="alignment button group">
+            <Button>left</Button>
+            <Button>center</Button>
+            <Button>right</Button>
+          </ButtonGroup>
+        </Stack>
+        <ToggleButtonGroup
+          aria-label="text formatting"
+          value={formats}
+          onChange={handleFormatChange}
+        >
+          <ToggleButton value="bold" aria-label="bold">
+            <FormatBoldIcon />
+          </ToggleButton>
+          <ToggleButton value="italic" aria-label="italic">
+            <FormatItalicIcon />
+          </ToggleButton>
+          <ToggleButton value="underlined" aria-label="underlined">
+            <FormatUnderlinedIcon />
+          </ToggleButton>
+        </ToggleButtonGroup>{" "}
+        <ToggleButtonGroup
+          aria-label="text formatting"
+          value={exclusiveFormats}
+          orientation="vertical"
+          exclusive
+          onChange={handleExclusiveFormatChange}
+        >
+          <ToggleButton value="bold" aria-label="bold">
+            <FormatBoldIcon />
+          </ToggleButton>
+          <ToggleButton value="italic" aria-label="italic">
+            <FormatItalicIcon />
+          </ToggleButton>
+          <ToggleButton value="underlined" aria-label="underlined">
+            <FormatUnderlinedIcon />
+          </ToggleButton>
+        </ToggleButtonGroup>
       </main>
     </>
   );
