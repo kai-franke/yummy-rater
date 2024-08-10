b3448efc059ff1c5dacd1baab1862849ce84c0bd
Kai Franke <cosinushh@gmail.com>
2024-08-10 11:09:09 +0200

    update model and schemas

--- models/User.tsx
+++ models/User.tsx
@@ -2,14 +2,26 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
+const productSchema = new Schema(
+  {
+    ean: { type: Number, required: true },
+    name: String,
+    brand: String,
+    description: String,
+    image: String,
+    user_rating: Number,
+    user_note: String,
+  },
+  { timestamps: true }
+);
+
 const userSchema = new Schema({
-  provider_id: { type: String, required: true },
+  provider_id: { type: String, required: true, index: true },
   first_name: String,
   last_name: String,
   profile_image: String,
   email: String,
-  // Muss products anders definiert werden?
-  products: Array,
+  products: [productSchema],
 });
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
