c0dc5b7bcfceb92f11f4d4ea59dbddf1c310761c
Kai Franke <cosinushh@gmail.com>
2024-09-07 19:33:09 +0200

    add correct components to Scan page

---
+++ pages/scan.tsx
@@ -0,0 +1,13 @@
+import { Box, Typography } from "@mui/material";
+
+export default function Scan() {
+  return (
+    <main>
+      <Box m={3} component="section">
+        <Typography variant="h5" component="h2">
+          Scan
+        </Typography>
+      </Box>
+    </main>
+  );
+}
