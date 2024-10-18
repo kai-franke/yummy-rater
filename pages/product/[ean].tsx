7f98c570fad3388a77bd155448cc61da1cb9a270
Kai Franke <cosinushh@gmail.com>
2024-10-18 18:30:34 +0200

    clean up code

--- pages/product/[ean].tsx
+++ pages/product/[ean].tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import { Alert, Typography } from "@mui/material";
-import { ISanitizedUser, IUser } from "@/types/user";
+import { IUser } from "@/types/user";
 import { IProduct } from "@/types/product";
 import ProductCard from "@/components/ProductCard";
 
@@ -14,25 +14,17 @@ export default function ProductPage({ userData }: ProductPageProps) {
   const productToShow = userData?.products?.find(
     (product: IProduct) => product.ean.toString() === ean
   );
-  console.log('productToShow', productToShow)
-
-  if (!productToShow) {
-    return (
-      <>
-        <Typography variant="h5" component="h2" gutterBottom>
-          Product Details
-        </Typography>
-        <Alert severity="error">No Product for EAN {`${ean}`} found.</Alert>
-      </>
-    );
-  }
 
   return (
     <>
       <Typography variant="h5" component="h2" gutterBottom>
         Product Details
       </Typography>
+      {productToShow ? (
         <ProductCard product={productToShow} />
+      ) : (
+        <Alert severity="error">No Product for EAN {`${ean}`} found.</Alert>
+      )}
     </>
   );
 }
