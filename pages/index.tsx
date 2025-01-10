508c2e2c8d8df6248bff5687d5c70f0c2503d155
Kai Franke <cosinushh@gmail.com>
2025-01-10 16:41:17 +0100

    add Modal component to index.tsx for testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,42 @@
-import { Typography } from "@mui/material";
+import Modal, { ModalAction } from "@/components/Modal";
+import { Button, Typography } from "@mui/material";
+import { useState } from "react";
 
 export default function Home() {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const actions: ModalAction[] = [
+    {
+      label: "Abbrechen",
+      onClick: () => setModalOpen(false),
+      variant: "text",
+      color: "secondary",
+    },
+    {
+      label: "Speichern",
+      onClick: () => {
+        /* speichern */ setModalOpen(false);
+      },
+      variant: "contained",
+      color: "primary",
+    },
+  ];
+
   return (
-    <Typography variant="h5" component="h2" gutterBottom>
-      Home
-    </Typography>
+    <>
+      {modalOpen && (
+        <Modal
+          open={modalOpen}
+          onClose={() => setModalOpen(false)}
+          title="Test Modal"
+          actions={actions}
+          content={<p>Hier kann beliebiger Inhalt stehen</p>}
+        ></Modal>
+      )}
+      <Typography variant="h5" component="h2" gutterBottom>
+        Home
+      </Typography>
+      <Button onClick={() => setModalOpen(true)}>Open Modal</Button>
+    </>
   );
 }
