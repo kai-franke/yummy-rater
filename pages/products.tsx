90d8f060bd244f91587a7f1abbd9542df8740683
Kai Franke <cosinushh@gmail.com>
2025-02-09 20:24:12 +0100

    create Products page

---
+++ pages/products.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+export default function Products() {
+  return (
+    <>
+      <Typography variant="h5" component="h2">
+        My Products
+      </Typography>
+    </>
+  );
+}
