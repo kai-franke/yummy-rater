84a35ea7d57b0aa7d358f5932379f39a903fa85b
Kai Franke <cosinushh@gmail.com>
2025-02-15 20:53:40 +0100

    add search functionality

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Fuse from "fuse.js";
 
 import Modal from "@/components/Modal";
 import ProductCard from "@/components/ProductCard";
@@ -9,10 +10,13 @@ import { ModalAction } from "@/types/modal";
 import {
   Box,
   Button,
+  IconButton,
+  InputAdornment,
   Paper,
   TableCell,
   TableRow,
   TableSortLabel,
+  TextField,
   Tooltip,
   Typography,
 } from "@mui/material";
@@ -25,15 +29,20 @@ import Rating from "@mui/material/Rating";
 import ImageNotSupportedIcon from "@mui/icons-material/ImageNotSupported";
 import CloseIcon from "@mui/icons-material/Close";
 import SearchOffIcon from "@mui/icons-material/SearchOff";
+import FilterAltIcon from "@mui/icons-material/FilterAlt";
+import FilterAltOffIcon from "@mui/icons-material/FilterAltOff";
+import ClearIcon from "@mui/icons-material/Clear";
 
 export default function Products({ userData }: PageProps) {
   const initialProducts = userData?.products || [];
-  const [products, setProducts] = useState<IProduct[]>(initialProducts);
+  const [displayedProducts, setDisplayedProducts] =
+    useState<IProduct[]>(initialProducts);
   const [sortConfig, setSortConfig] = useState<{
     key: keyof IProduct;
     direction: "asc" | "desc";
   }>({ key: "createdAt", direction: "desc" });
-  const [selectedProduct, setSelectedProduct] = useState(products[0]);
+  const [selectedProduct, setSelectedProduct] = useState(displayedProducts[0]);
+  const [filterTerm, setFilterTerm] = useState("");
   const [modalOpen, setModalOpen] = useState(false);
   const modalActions: ModalAction[] = [
     {
@@ -44,13 +53,12 @@ export default function Products({ userData }: PageProps) {
     },
   ];
 
-  // initiale Sortierung nach Erstellungsdatum
   useEffect(() => {
-    sortProducts("createdAt", "desc");
-  }, []);
+    sortProducts(sortConfig.key, sortConfig.direction);
+  }, [displayedProducts]);
 
   function sortProducts(key: keyof IProduct, direction: "asc" | "desc") {
-    const sortedProducts = [...products].sort((a, b) => {
+    const sortedProducts = [...displayedProducts].sort((a, b) => {
       let valA = a[key];
       let valB = b[key];
 
@@ -66,7 +74,7 @@ export default function Products({ userData }: PageProps) {
       if (valA > valB) return direction === "asc" ? 1 : -1;
       return 0;
     });
-    setProducts(sortedProducts);
+    setDisplayedProducts(sortedProducts);
   }
 
   function handleSort(key: keyof IProduct, direction?: "asc" | "desc") {
@@ -83,6 +91,21 @@ export default function Products({ userData }: PageProps) {
     setModalOpen(true);
   }
 
+  useEffect(() => {
+    if (!filterTerm) {
+      setDisplayedProducts(initialProducts);
+    } else {
+      const fuse = new Fuse(initialProducts, {
+        keys: ["name", "brand", "ean", "description", "user_note"],
+        threshold: 0.3, // Je niedriger der Wert, desto strenger die Suche
+      });
+
+      const results = fuse.search(filterTerm);
+
+      setDisplayedProducts(results.map((result) => result.item));
+    }
+  }, [filterTerm]);
+
   return (
     <>
       <Typography variant="h5" component="h2" gutterBottom>
@@ -90,7 +113,36 @@ export default function Products({ userData }: PageProps) {
       </Typography>
 
       <Paper>
-        <Box sx={{ display: "flex", justifyContent: "flex-end", padding: 1 }}>
+        <Box
+          sx={{ display: "flex", justifyContent: "space-between", padding: 1 }}
+        >
+          <TextField
+            sx={{ my: 1, minWidth: "320px" }}
+            label="Filter"
+            placeholder="e.g. Applepie or 4478738324"
+            variant="outlined"
+            size="small"
+            value={filterTerm}
+            onChange={(e) => setFilterTerm(e.target.value)}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  {filterTerm ? (
+                    <FilterAltIcon fontSize="small" />
+                  ) : (
+                    <FilterAltOffIcon fontSize="small" color="disabled" />
+                  )}
+                </InputAdornment>
+              ),
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton onClick={() => setFilterTerm("")} size="small">
+                    {<ClearIcon fontSize="small" />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
           <Button
             onClick={() => handleSort("createdAt", "desc")}
             disabled={sortConfig.key === "createdAt"}
@@ -142,7 +194,7 @@ export default function Products({ userData }: PageProps) {
               </TableRow>
             </TableHead>
             <TableBody>
-              {!products || products.length === 0 ? (
+              {!displayedProducts || displayedProducts.length === 0 ? (
                 <TableRow>
                   <TableCell colSpan={5} align="center" sx={{ py: 10 }}>
                     <SearchOffIcon color="disabled" fontSize="large" />
@@ -152,7 +204,7 @@ export default function Products({ userData }: PageProps) {
                   </TableCell>
                 </TableRow>
               ) : (
-                products.map((product) => (
+                displayedProducts.map((product) => (
                   <TableRow
                     key={product.ean}
                     onClick={() => handleProductClick(product)}
