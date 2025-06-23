27793bf658130f44cb3469a620edf9379454b27c
Kai Franke <cosinushh@gmail.com>
2025-06-23 08:01:05 +0200

    create basic theme

---
+++ styles/theme.ts
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#7B7773",
+        },
+        secondary: {
+            main: "#F0D755",
+        },
+        background: {
+            default: "#F4F2F1",
+            paper: "#ffffff",
+        },
+    },
+})
+
+export default theme;
