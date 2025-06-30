54bca4b0fe81c360a0846b3b0650c0a5907f0c88
Kai Franke <cosinushh@gmail.com>
2025-06-30 21:24:21 +0200

    change color of active icon in BottomNavigation

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,23 +1,31 @@
 import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
-    palette: {
-        primary: {
-            main: "#7B7773",
-        },
-        secondary: {
-            main: "#F0D755",
-        },
-        background: {
-            default: "#F4F2F1",
-            paper: "#ffffff",
-        },
+  palette: {
+    primary: {
+      main: "#7B7773",
     },
-    components:{
-        MuiBottomNavigationAction:{
-            
-        }
+    secondary: {
+      main: "#F0D755",
     },
-})
+    background: {
+      default: "#F4F2F1",
+      paper: "#ffffff",
+    },
+  },
+  components: {
+    MuiBottomNavigationAction: {
+      styleOverrides: {
+        root: {
+          "&.Mui-selected": {
+            "& .MuiSvgIcon-root": {
+              color: "#F0D755",
+            },
+          },
+        },
+      },
+    },
+  },
+});
 
 export default theme;
