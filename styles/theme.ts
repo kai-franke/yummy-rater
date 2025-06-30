765519cb9c95efb371b57aeafaec92a1bfb32e28
Kai Franke <cosinushh@gmail.com>
2025-06-30 21:39:20 +0200

    replace hard-coded color value with variable

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,5 +1,34 @@
 import { createTheme } from "@mui/material/styles";
 
+// const theme = createTheme({
+//   palette: {
+//     primary: {
+//       main: "#7B7773",
+//     },
+//     secondary: {
+//       main: "#F0D755",
+//     },
+//     background: {
+//       default: "#F4F2F1",
+//       paper: "#ffffff",
+//     },
+//   },
+// });
+
+// theme.components = {
+//     MuiBottomNavigationAction: {
+//       styleOverrides: {
+//         root: {
+//           "&.Mui-selected": {
+//             "& .MuiSvgIcon-root": {
+//               color: theme.palette.secondary.main,
+//             },
+//           },
+//         },
+//       },
+//     },
+//   };
+
 const theme = createTheme({
   palette: {
     primary: {
@@ -16,13 +45,11 @@ const theme = createTheme({
   components: {
     MuiBottomNavigationAction: {
       styleOverrides: {
-        root: {
-          "&.Mui-selected": {
-            "& .MuiSvgIcon-root": {
-              color: "#F0D755",
-            },
+        root: ({ theme }) => ({
+          "&.Mui-selected .MuiSvgIcon-root": {
+            color: theme.palette.secondary.main,
           },
-        },
+        }),
       },
     },
   },
