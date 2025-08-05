78de6456d62c6b781e669e13a1deba0947064732
Kai Franke <cosinushh@gmail.com>
2025-08-05 19:02:28 +0200

    clean up theme.ts

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,32 +1,5 @@
 import { createTheme } from "@mui/material/styles";
 
-// const theme = createTheme({
-//   palette: {
-//     primary: {
-//       main: "#7B7773",
-//     },
-//     secondary: {
-//       main: "#F0D755",
-//     },
-//     background: {
-//       default: "#F4F2F1",
-//       paper: "#ffffff",
-//     },
-//   },
-// });
-
-// theme.components = {
-//     MuiBottomNavigationAction: {
-//       styleOverrides: {
-//         root: {
-//           "&.Mui-selected .MuiSvgIcon-root": {
-//             color: theme.palette.secondary.main,
-//           },
-//         },
-//       },
-//     },
-//   };
-
 const theme = createTheme({
   palette: {
     primary: {
