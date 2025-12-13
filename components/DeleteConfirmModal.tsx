9be3ff44b718cdd2d3b6d0035b62264d50b625a7
Kai Franke <cosinushh@gmail.com>
2025-12-13 14:56:43 +0100

    create DeleteConfirmModal component

---
+++ components/DeleteConfirmModal.tsx
@@ -0,0 +1,57 @@
+import Modal from "@/components/Modal";
+import { Typography, Box } from "@mui/material";
+import { IProduct } from "@/types/product";
+
+type Props = {
+  open: boolean;
+  product: IProduct | null;
+  loading?: boolean;
+  onCancel: () => void;
+  onConfirm: () => void;
+};
+
+export default function DeleteConfirmModal({
+  open,
+  product,
+  loading,
+  onCancel,
+  onConfirm,
+}: Props) {
+  if (!product) return null;
+
+  return (
+    <Modal
+      open={open}
+      onClose={onCancel}
+      title="Confirm Deletion"
+      actions={[
+        {
+          label: "Cancel",
+          variant: "outlined",
+          onClick: onCancel,
+        },
+        {
+          label: "Delete",
+          variant: "contained",
+          color: "error",
+          onClick: onConfirm,
+          disabled: loading,
+        },
+      ]}
+    >
+      <Typography>
+        Do you really want to delete the product{" "}
+        <Box
+          component="span"
+          sx={{ fontWeight: "bold", color: "primary.main" }}
+        >
+          {product?.name}
+        </Box>{" "}
+        permanently?
+      </Typography>
+      <Typography color="error.main" mt={1}>
+        This action cannot be undone.
+      </Typography>
+    </Modal>
+  );
+}
